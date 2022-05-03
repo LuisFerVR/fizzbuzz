a6360d179b46b6a013aa2ea555f8eda86102a544
LuisFerVR <luis.fer.vera.ruiz@gmail.com>
2022-05-02 19:31:54 -0500

    diseño de prueba

--- test/services/ExplorerService.test.js
+++ test/services/ExplorerService.test.js
@@ -1,10 +1,19 @@
 const ExplorerService = require("./../../lib/services/ExplorerService");
+const Reader = require("../../lib/services/ExplorerService");
 
 describe("Tests para ExplorerService", () => {
     test("Requerimiento 1: Calcular todos los explorers en una misión", () => {
         const explorers = [{mission: "node"}];
         const explorersInNode = ExplorerService.filterByMission(explorers, "node");
         expect(explorersInNode.length).toBe(1);
+    }),
+    test("Contribución Open Source, Requerimiento nuevo: Crea un endpoint nuevo que regrese toda la lista de explorers filtrados por un stack", () => {
+        const explorers = [
+            {"name": "Wopaa1","stacks": ["reasonML","elm"]},
+            {"name": "Wopaa2","stacks": ["javascript","elixir"]}];
+    
+        const explorersInJs = ExplorerService.filterByStack(explorers, "javascript");
+        expect(explorersInJs).toStrictEqual( [{"name": "Wopaa2","stacks": ["javascript","elixir"]}]);
     });
 
 });
